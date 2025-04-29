5df9e76bc196fcea9eabdf72980fa1295c432639
Emilia Flisikowska <flisikowska.emilia@gmail.com>
2025-04-29 12:20:26 +0200

    Added consumptionCapacity to Tavern class

--- src/GeneratorDanych/TavernFactory.java
+++ src/GeneratorDanych/TavernFactory.java
@@ -7,6 +7,7 @@ import java.util.Random;
 class TavernFactory implements IFactory<Tavern> {
     @Override
     public Tavern Create(Random random, int x, int y) {
-        return new Tavern(x, y);
+        double consCap = random.nextDouble(10, 10000);
+        return new Tavern(x, y, consCap);
     }
 }
--- src/SiecPrzeplywowa/Tavern.java
+++ src/SiecPrzeplywowa/Tavern.java
@@ -3,7 +3,15 @@ package SiecPrzeplywowa;
 import java.awt.geom.Point2D;
 
 public class Tavern extends Point2D.Double{
-    public Tavern(int x, int y) {
+    private double consumptionCapacity;
+
+    public Tavern(int x, int y, double consumptionCapacity) {
         super(x, y);
+        this.consumptionCapacity=consumptionCapacity;
     }
+
+    public double getConsumptionCapacity() {
+        return consumptionCapacity;
+    }
+
 }
