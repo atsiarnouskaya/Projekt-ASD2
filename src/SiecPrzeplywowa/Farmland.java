0bb60d1367b06065b29576d5bf3a4e05be312132
julcziqqx <jula.brdow.12@gmail.com>
2025-05-19 22:47:12 +0200

    Added Quadrant class with Graham hull algorithm implemented

--- src/SiecPrzeplywowa/Farmland.java
+++ src/SiecPrzeplywowa/Farmland.java
@@ -7,7 +7,7 @@ public class Farmland extends Point2D.Double {
 
     public Farmland(int x, int y, int productionCapacity) {
         super(x, y);
-        this.productionCapacity = productionCapacity;
+        this.productionCapacity = productionCapacity; // productionCapacity will depend on quadrant
     }
 
     public int getProductionCapacity() {
